862ae41ee2bef2a3d67d16b70787200a0f0e8ed9
flautrup <fredrik.lautrup@qlik.com>
2017-07-11 14:27:31 +0200

    Able to decode header to get which function to execute.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const grpc = require('grpc');
+var protobuf = require("protobufjs");
+var builder=protobuf.loadProtoFile('proto/ServerSideExtension.proto');
+var model=builder.build("qlik.sse");
 
 const proto = grpc.load('proto/ServerSideExtension.proto');
 const server = new grpc.Server();
@@ -9,18 +12,49 @@ const GetCapabilities = (call, callback) => {
         pluginIdentifier: 'secSSE',
         pluginVersion: 'v0.0.1',
         functions: [{
-            functionId: 0,
-            name: 'HelloWorld',
-            functionType: 0,
-            returnType: 0
-        }]
+                functionId: 0,
+                name: 'HelloWorld',
+                functionType: proto.qlik.sse.FunctionType.TENSOR,
+                returnType: proto.qlik.sse.DataType.STRING,
+                params: [{
+                    name: 'str1',
+                    dataType: proto.qlik.sse.DataType.STRING
+                }]
+            },
+            {
+                functionId: 1,
+                name: 'HelloWorld2',
+                functionType: proto.qlik.sse.FunctionType.SCALAR,
+                returnType: proto.qlik.sse.DataType.STRING,
+                params: [{
+                    name: 'str1',
+                    dataType: proto.qlik.sse.DataType.STRING
+                }]
+            }
+        ]
     };
 
-    callback(null,capabilities);
+    callback(null, capabilities);
 }
 
-const ExecuteFunction = (call, callback) => {
-    return "Test";
+
+const ExecuteFunction = (call) => {
+    var requestHeaders = call.metadata.getMap();
+    var functionRequestHeader = requestHeaders['qlik-functionrequestheader-bin'];
+    var commonRequestHeader = requestHeaders['qlik-commonrequestheader-bin'];
+
+    var header=model.FunctionRequestHeader.decode(functionRequestHeader);
+
+    call.on('data', function (rowData) {
+        for (count = 0; count++; count <= rowData.rows.length) {
+            rowData.rows[0].duals[0].strData = 'Hello World';
+            rowData.rows[0].duals[0].numData = 1;
+        }
+        call.write(rowData);
+    });
+    call.on('end', function () {
+        call.end();
+    });
 }
 
 const EvaluateScript = (call, callback) => {
