ccf95324a1991543356391843756b004dcea3f19
flautrup <fredrik.lautrup@qlik.com>
2017-07-12 09:56:50 +0200

    Started to add functionality for multiple functions

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const grpc = require('grpc');
 var protobuf = require("protobufjs");
+// load proto model to be able to decode header to determine function called
 var builder=protobuf.loadProtoFile('proto/ServerSideExtension.proto');
-var model=builder.build("qlik.sse");
+var protoModel=builder.build("qlik.sse");
 
+// load proto for grpc service
 const proto = grpc.load('proto/ServerSideExtension.proto');
 const server = new grpc.Server();
 
+// send back capabilties to Qlik Sense 
 const GetCapabilities = (call, callback) => {
     var capabilities = {
         allowScript: false,
@@ -14,16 +17,6 @@ const GetCapabilities = (call, callback) => {
         functions: [{
                 functionId: 0,
                 name: 'HelloWorld',
-                functionType: proto.qlik.sse.FunctionType.TENSOR,
-                returnType: proto.qlik.sse.DataType.STRING,
-                params: [{
-                    name: 'str1',
-                    dataType: proto.qlik.sse.DataType.STRING
-                }]
-            },
-            {
-                functionId: 1,
-                name: 'HelloWorld2',
                 functionType: proto.qlik.sse.FunctionType.SCALAR,
                 returnType: proto.qlik.sse.DataType.STRING,
                 params: [{
@@ -37,18 +30,27 @@ const GetCapabilities = (call, callback) => {
     callback(null, capabilities);
 }
 
-
+// function to route requests to right function
 const ExecuteFunction = (call) => {
     var requestHeaders = call.metadata.getMap();
     var functionRequestHeader = requestHeaders['qlik-functionrequestheader-bin'];
     var commonRequestHeader = requestHeaders['qlik-commonrequestheader-bin'];
 
-    var header=model.FunctionRequestHeader.decode(functionRequestHeader);
+    var header=protoModel.FunctionRequestHeader.decode(functionRequestHeader);
 
     call.on('data', function (rowData) {
-        for (count = 0; count++; count <= rowData.rows.length) {
-            rowData.rows[0].duals[0].strData = 'Hello World';
-            rowData.rows[0].duals[0].numData = 1;
+        // choose function based on header. 
+        //Improvement to drive this from definition
+        if(header.functionId==0) {
+            rowData=helloWorld(rowData);
+        } else if (header.functionId==1) {
+            rowData=aesencryptData(rowData);
+        } else if (header.functionId==2) {
+            rowData=aesDecryptData(rowData);
+        } else if (header.functionId==3) {
+            rowData=fpeEncryptData(rowData);
+        } else if (header.functionId==4) {
+            rowData.fpeDecryptData(rowData);
         }
         call.write(rowData);
     });
@@ -62,8 +64,12 @@ const EvaluateScript = (call, callback) => {
 }
 
 
-const helloWorld = () => {
-    return "helloWorld";
+const helloWorld = (rowData) => {
+    for(count=0;  count < rowData.rows.length; count++) {
+         rowData.rows[count].duals[0].strData = 'Hello World';
+         rowData.rows[count].duals[0].numData = 1;
+    }
+    return rowData;
 }
 
 
